26f9d8fd7f301465fbb91549712bc0f64247ae5e
Jerry Tram <minh.tram@mavs.uta.edu>
2019-02-08 19:05:13 -0600

    Added RecyclerView
    integrated with master. Currently hardcoded recyclerview.

--- app/src/main/java/me/koltensturgill/sloth/MainActivity.java
+++ app/src/main/java/me/koltensturgill/sloth/MainActivity.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -17,6 +19,9 @@ import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener, View.OnClickListener {
@@ -24,6 +29,12 @@ public class MainActivity extends AppCompatActivity
     public static int _CREATE = 1;
 
     EditText editText;
+
+    List<String> input; //hardcoded sample list for RecyclerView
+
+    private RecyclerView recyclerView;
+    private RecyclerAdapter recyclerAdapter;
+    private RecyclerView.LayoutManager recyclerLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,9 +52,28 @@ public class MainActivity extends AppCompatActivity
             public void onClick(View view) {
             // Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
             // .setAction("Action", null).show();
+
+            //recycler adding
+            recyclerAdapter = (RecyclerAdapter) recyclerView.getAdapter();
+            recyclerAdapter.getValues().add("New Item");
+            recyclerAdapter.notifyItemInserted(input.size()-1);
+
             startActivityForResult(intent, _CREATE);
             }
         });
+
+        //Jerp stuff for RecyclerView =================
+
+        recyclerView = (RecyclerView)findViewById(R.id.recycler_main);
+        recyclerView.setHasFixedSize(true);
+        recyclerLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(recyclerLayoutManager);
+        input = new ArrayList<>();
+        for (int i = 0; i < 20; i++) input.add("Note " + i);
+        recyclerAdapter = new RecyclerAdapter(input);
+        recyclerView.setAdapter(recyclerAdapter);
+
+        //=============================================
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -87,6 +117,9 @@ public class MainActivity extends AppCompatActivity
         return super.onOptionsItemSelected(item);
     }
 
+    //ORIGINAL NAV HANDLER
+
+    /*
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
@@ -95,6 +128,42 @@ public class MainActivity extends AppCompatActivity
 
         if (id == R.id.nav_camera) {
             // Handle the camera action
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+    */
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_folderA)
+        {
+            List<String> newsublist = new ArrayList<>();
+            for(int i = 0; i < 5; i++) newsublist.add("List A note " + i);
+            recyclerView.setAdapter(new RecyclerAdapter(newsublist));
+        }
+        else if (id == R.id.nav_folderB)
+        {
+            List<String> newsublist = new ArrayList<>();
+            for(int i = 0; i < 2; i++) newsublist.add("List B note " + i);
+            recyclerView.setAdapter(new RecyclerAdapter(newsublist));
+        }
+        else if (id == R.id.nav_folderC)
+        {
+            List<String> newsublist = new ArrayList<>();
+            for(int i = 0; i < 7; i++) newsublist.add("List C note " + i);
+            recyclerView.setAdapter(new RecyclerAdapter(newsublist));
+        }
+        else if (id == R.id.nav_folderD)
+        {
+            List<String> newsublist = new ArrayList<>();
+            recyclerView.setAdapter(new RecyclerAdapter(newsublist));
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
